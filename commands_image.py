a04de61f9da909e1e96335b47930a042e6c69ed6
Brayconn <29030756-Brayconn@users.noreply.github.com>
2020-01-27 15:35:07 -0800

    Refactored images

--- commands_image.py
+++ commands_image.py
@@ -1,97 +1,70 @@
 import discord
 from smath import *
 
+class image_sender:
+    is_command = true
 
-class how:
-    is_command = True
-
-    def __init__(self):
-        self.name = ["how.gif"]
-        self.minm = 0
-        self.desc = "Sends HOW.gif in the current chat."
-        self.usag = "<verbose:(?v)>"
+    def __init__(self, name, minm, url, desc):
+        self.name = name
+        self.minm = minm
+        self.url = url
+        self.desc = desc
+        seld.usag = "<verbose:(?v)>"
 
     async def __call__(self, flags, channel, **void):
-        url = "https://media.discordapp.net/attachments/500919580596764673/642515924578205696/HOW.gif"
         if "v" in flags:
-            return url
-        emb = discord.Embed(url=url)
-        emb.set_image(url=url)
-        print(url)
+            return self.url
+        emb = discord.Embed(url=self.url)
+        emb.set_image(url=self.url)
+        print(self.url)
         await channel.send(embed=emb)
+
+class how(image_sender):
+    def __init__(self):
+        image_sender.__init__(self,
+                              ["how.gif"],
+                              0,
+                              "https://media.discordapp.net/attachments/500919580596764673/642515924578205696/HOW.gif",
+                              "Sends HOW.gif in the current chat."
+                              )
+
 
         
-class ded:
-    is_command = True
-
+class ded(image_sender):
     def __init__(self):
-        self.name = ["ded.gif","4aa217e","4aa217e.gif"]
-        self.minm = 0
-        self.desc = "Sends 4aa217e.gif in the current chat."
-        self.usag = "<verbose:(?v)>"
-
-    async def __call__(self, flags, channel, **void):
-        url = "https://cdn.discordapp.com/attachments/430005329984487434/654326757654134785/4aa217e.gif"
-        if "v" in flags:
-            return url
-        emb = discord.Embed(url=url)
-        emb.set_image(url=url)
-        print(url)
-        await channel.send(embed=emb)
+        image_sender.__init__(self,
+                              ["ded.gif","4aa217e","4aa217e.gif"],
+                              0,
+                              "https://cdn.discordapp.com/attachments/430005329984487434/654326757654134785/4aa217e.gif",
+                              "Sends 4aa217e.gif in the current chat."
+                              )
 
 
-class parrot:
-    is_command = True
-
+class parrot(image_sender):
     def __init__(self):
-        self.name = ["parrot.gif"]
-        self.minm = 0
-        self.desc = "Sends parrot.gif in the current chat."
-        self.usag = "<verbose:(?v)>"
-
-    async def __call__(self, flags, channel, **void):
-        url = "https://media.discordapp.net/attachments/543449246221860885/651594997925281803/parrot.gif"
-        if "v" in flags:
-            return url
-        emb = discord.Embed(url=url)
-        emb.set_image(url=url)
-        print(url)
-        await channel.send(embed=emb)
+        image_sender.__init__(self,
+                              ["parrot.gif"],
+                              0,
+                              "https://media.discordapp.net/attachments/543449246221860885/651594997925281803/parrot.gif",
+                              "Sends parrot.gif in the current chat."
+                              )
 
 
-class dolphino:
-    is_command = True
-
+class dolphino(image_sender):
     def __init__(self):
-        self.name = ["dolphino.gif","dolphin.gif","dolphin"]
-        self.minm = 0
-        self.desc = "Sends dolphino.gif in the current chat."
-        self.usag = "<verbose:(?v)>"
-
-    async def __call__(self, flags, channel, **void):
-        url = "https://cdn.discordapp.com/attachments/320915703102177293/671169395019612170/dolphino.gif"
-        if "v" in flags:
-            return url
-        emb = discord.Embed(url=url)
-        emb.set_image(url=url)
-        print(url)
-        await channel.send(embed=emb)
+        image_sender.__init__(self,
+                              ["dolphino.gif","dolphin.gif","dolphin"],
+                              0,
+                              "https://cdn.discordapp.com/attachments/320915703102177293/671169395019612170/dolphino.gif",
+                              "Sends dolphino.gif in the current chat."
+                              )
 
 
-class curiouseal:
-    is_command = True
-
+class curiouseal(image_sender):
     def __init__(self):
-        self.name = ["curiouseal.gif","seal.gif","seal"]
-        self.minm = 0
-        self.desc = "Sends curiouseal.gif in the current chat."
-        self.usag = "<verbose:(?v)>"
-
-    async def __call__(self, flags, channel, **void):
-        url = "https://cdn.discordapp.com/attachments/320915703102177293/671167411914801182/curiouseal.gif"
-        if "v" in flags:
-            return url
-        emb = discord.Embed(url=url)
-        emb.set_image(url=url)
-        print(url)
-        await channel.send(embed=emb)
+        image_sender.__init__(self,
+                              ["curiouseal.gif","seal.gif","seal"],
+                              0,
+                              "https://cdn.discordapp.com/attachments/320915703102177293/671167411914801182/curiouseal.gif",
+                              "Sends curiouseal.gif in the current chat."
+                              )
