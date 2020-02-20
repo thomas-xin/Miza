ab478e70b513a1ad08f90d0d43bef06aadbff310
Thomas Xin <thomasxin@gmail.com>
2020-02-21 03:47:30 +1100

    fixed flag detection crash.

--- bot.py
+++ bot.py
@@ -681,6 +681,7 @@ async def processMessage(message, msg, edit=True, orig=None, cb_argv=None, cb_fl
                                     char = chr(c + 97)
                                     flag = "?" + char
                                     for r in (flag, flag.upper()):
+                                        found = False
                                         while len(argv) >= 4 and r in argv:
                                             i = argv.index(r)
                                             if i == 0 or argv[i - 1] == " " or argv[i - 2] == "?":
@@ -688,21 +689,29 @@ async def processMessage(message, msg, edit=True, orig=None, cb_argv=None, cb_fl
                                                     if argv[i + 2] == " " or argv[i + 2] == "?":
                                                         argv = argv[:i] + argv[i + 2:]
                                                         addDict(flags, {char: 1})
+                                                        found = True
                                                 except (IndexError, KeyError):
                                                     pass
+                                            if not found:
+                                                break
                             if "?" in argv:
                                 for c in range(26):
                                     char = chr(c + 97)
                                     flag = "?" + char
                                     for r in (flag, flag.upper()):
+                                        found = False
                                         while len(argv) >= 2 and r in argv:
                                             for check in (r + " ", " " + r):
                                                 if check in argv:
                                                     argv = argv.replace(check, "")
                                                     addDict(flags, {char: 1})
+                                                    found = True
                                             if argv == r:
                                                 argv = ""
                                                 addDict(flags, {char: 1})
+                                                found = True
+                                            if not found:
+                                                break
                         if argv:
                             while argv[0] == " ":
                                 argv = argv[1:]
