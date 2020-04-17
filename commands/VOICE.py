31f487b1d8f1b28aeb2af82d6cff1b78e8f2e077
Thomas Xin <thomasxin@gmail.com>
2020-04-17 16:18:22 +1000

    fixed syntax error in sending from thread.

--- commands/VOICE.py
+++ commands/VOICE.py
@@ -298,15 +298,12 @@ class customAudio(discord.AudioSource):
                     break
                 e = q[i]
                 if not e.url:
-                    create_task(
-                        self._vars.sendReact(
-                            self.channel,
-                            "```ini\nA problem occured while loading " + sbHighlight(e.name)
-                            + ", and it has been removed from the queue as a result.```",
-                            reacts=["❎"],
-                        ),
-                        loop=self.loop,
-                    )
+                    create_task(sendReact(
+                        self.channel,
+                        "```ini\nA problem occured while loading " + sbHighlight(e.name)
+                        + ", and it has been removed from the queue as a result.```",
+                        reacts=["❎"],
+                    ), loop=self.loop)
                     dels.append(i)
                     continue
                 h = gethash(e)
