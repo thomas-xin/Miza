e3bc9e103ef30b85168cda9a6701042c5edcb25c
Thomas Xin <thomasxin@gmail.com>
2022-10-25 01:10:20 +1100

    typo

--- commands/MISC.py
+++ commands/MISC.py
@@ -970,7 +970,7 @@ class StableDiffusion(Command):
         if specified:
             req += " ".join(f"{k} {v}" for k, v in kwargs.items() if k in specified)
         fn = None
-        if not specified and (xrand(2) and not url and or not os.path.exists("misc/stable_diffusion.openvino")):
+        if not specified and (xrand(2) and not url or not os.path.exists("misc/stable_diffusion.openvino")):
             if self.cache.get(prompt):
                 b = io.BytesIO()
                 self.cache[prompt].pop(0).save(b, format="png")
