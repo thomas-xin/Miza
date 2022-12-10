0de84d50e9006ab0d6884b866ff0053236d2c252
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2022-12-11 05:39:44 +1100

    Update install_update.py

--- install_update.py
+++ install_update.py
@@ -78,7 +78,7 @@ except:
 
 try:
     v = pkg_resources.get_distribution("yt_dlp").version
-    assert v >= "2022.8.19"
+    assert v >= "2022.11.11"
 except:
     print_exc()
     subprocess.run([python, "-m", "pip", "install", "git+https://github.com/yt-dlp/yt-dlp.git", "--upgrade", "--user"])
