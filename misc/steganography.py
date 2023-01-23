905e1e30745d33269e1daebfa72ab3bb20459ae6
Thomas Xin <thomasxin@gmail.com>
2023-01-23 23:34:03 +1100

    removed unnecessary prints

--- misc/steganography.py
+++ misc/steganography.py
@@ -173,7 +173,7 @@ def compare_to(im, msg):
 			h, l, s, g = map(np.float32, (h, l, s, g))
 			h2, l2, s2, g2 = map(np.float32, (h2, l2, s2, g2))
 			heff = np.sqrt(900 - (30 - s) * (30 - s2)) / 30
-			print(np.abs(np.abs(h - h2) - 8))
+			# print(np.abs(np.abs(h - h2) - 8))
 			hd = np.sum((8 - np.abs(np.abs(h - h2) - 8)) * heff) / 8 / 1024
 			seff = np.sqrt(900 - (30 - l) * (30 - l2)) / 30
 			sd = np.sum(np.abs(s - s2) * seff) / 15 / 1024
@@ -183,11 +183,11 @@ def compare_to(im, msg):
 			else:
 				gd = np.sum(np.abs(g - g2)) / 15 / 1024
 				ld = np.sum(np.abs(l - l2)) / 15 / 1024
-			print(np.abs(l - l2))
+			# print(np.abs(l - l2))
 
-			print(hd, sd, ld, gd, invert)
+			# print(hd, sd, ld, gd, invert)
 			R = (hd + sd) / 2 + min(ld, gd * 2)
-			print(rh, R, v)
+			# print(rh, R, v)
 			if R <= 0.05:
 				if v == msg:
 					print("No copyright detected.")
