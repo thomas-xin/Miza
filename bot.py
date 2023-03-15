61d95df7743cb44f1e15ee771dd1a31191731da8
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-03-16 08:54:20 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5986,7 +5986,7 @@ def as_file(file, filename=None, ext=None, rename=True):
         n = (ts_us() * random.randint(1, time.time_ns() % 65536) ^ random.randint(0, 1 << 63)) & (1 << 64) - 1
         key = base64.urlsafe_b64encode(n.to_bytes(8, "little")).rstrip(b"=").decode("ascii")
         create_task(Request(
-            f"https://127.0.0.1:{PORT}/api_register_replacer?fn={out}&key={key}",
+            f"https://127.0.0.1:{PORT}/api_register_replacer?ts={out}&key={key}",
             method="PUT",
             aio=True,
             ssl=False,
