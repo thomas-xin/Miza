6a490f47ec3a7a8db8aaef745a554d5b4834810a
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-03-23 17:49:01 +1100

    Update common.py

--- common.py
+++ common.py
@@ -2130,13 +2130,13 @@ async def sub_submit(ptype, command, fix=None, _timeout=12):
         try:
             proc.stdin.write(s)
             await proc.stdin.drain()
-            fut = wrap_future(PROC_RESP[ts])
+            fut = PROC_RESP[ts]
             tries = ceil(_timeout / 3)
             for i in range(tries):
                 if ts not in PROC_RESP:
                     raise ConnectionResetError("Response disconnected.")
                 try:
-                    resp = await asyncio.wait_for(fut, timeout=3)
+                    resp = await asyncio.wait_for(wrap_future(fut), timeout=3)
                 except T1:
                     if i >= tries - 1:
                         raise
