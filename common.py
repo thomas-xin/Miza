75f2226f8fd54937091a3487a20a6a6c273dfa23
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-03-29 13:10:46 +1100

    Update common.py

--- common.py
+++ common.py
@@ -788,7 +788,7 @@ class FileHashDict(collections.abc.MutableMapping):
                     with tracebacksuppressor:
                         d = self.pop(k, force=True)
                         self.deleted.discard(k)
-                        deleted.add(key)
+                        deleted.append(key)
                         d = select_and_dumps(d, mode="unsafe", compress=True)
                         self.cur.execute(
                             f"INSERT INTO '{self.path}' ('key', 'value') VALUES ('{k}', ?) "
