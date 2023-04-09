33a742f9c1ab8e8291b292e65dcfaccdc21e5230
Thomas Xin <thomasxin@gmail.com>
2023-04-09 22:21:41 +1000

    oops

--- misc/x-server.py
+++ misc/x-server.py
@@ -226,7 +226,6 @@ def error_handler(exc=None):
 	status = error_map.get(exc) or error_map.get(exc.__class__) or 500
 	resp = errdata.get(status) or errdata.setdefault(status, reqs.next().get(f"https://http.cat/{status}"))
 	head = resp.headers
-	head.update(cp.response.headers)
 	head.update(HEADERS)
 	cp.response.status = status
 	cp.response.headers.update(head)
