2dc4bc48194831b54f3a4c2b6171d8999573f02a
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-04-17 23:16:43 +1000

    Update convobot.py

--- misc/convobot.py
+++ misc/convobot.py
@@ -1070,7 +1070,7 @@ class Bot:
 							flagged = resp["results"][0]["flagged"]
 						except:
 							flagged = False
-						if not flagged:
+						if not flagged and not searched:
 							if nstart:
 								ns2 = "Assume y" + nstart[1:] + "\n"
 							else:
