d88d025ee5025b7ab35f03b8e895b4759b248be2
Thomas Xin <thomasxin@gmail.com>
2023-04-20 03:38:06 +1000

    multiple redirects

--- misc/x-server.py
+++ misc/x-server.py
@@ -178,20 +178,23 @@ class EndpointRedirects(Dispatcher):
 	def __call__(self, path):
 		send("@@@", escape=False)
 		p = path.lstrip("/")
-		if p == "ip":
-			p = "get_ip"
-		elif p[:3] == "/f/":
-			p = "/raw/" + p[3:]
-		elif p == "/upload":
-			p = "/files"
-		elif p == "/api/mpinsights":
-			p = "/api_mpinsights"
-		elif p.startswith("api/"):
-			p = p.removeprefix("api/")
-		elif p in actually_static:
-			p = "static/" + p
-		elif p in mapped_static:
-			p = "static/" + mapped_static[p]
+		while p:
+			if p == "ip":
+				p = "get_ip"
+			elif p[:2] == "f/":
+				p = "/raw/" + p[3:]
+			elif p == "/upload":
+				p = "/files"
+			elif p == "/api/mpinsights":
+				p = "/api_mpinsights"
+			elif p.startswith("api/"):
+				p = p.removeprefix("api/")
+			elif p in actually_static:
+				p = "static/" + p
+			elif p in mapped_static:
+				p = "static/" + mapped_static[p]
+			else:
+				break
 		p = "/" + p
 		if p != path:
 			print("REDIR:", path, p)
