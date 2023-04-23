21a38d8c10a6a1750ec0f2802f4619a6e84fd6f9
Thomas Xin <thomasxin@gmail.com>
2023-04-23 21:44:12 +1000

    split main url

--- misc/x-server.py
+++ misc/x-server.py
@@ -281,7 +281,7 @@ if os.path.exists("domain.cert.pem") and os.path.exists("private.key.pem"):
 	def hostmap(func):
 		def decorator(*args, **kwargs):
 			url = cp.url(qs=cp.request.query_string)
-			if not url.startswith("https://mizabot.") and not url.startswith("https://i.mizabot."):
+			if not url.startswith("https://mizabot.") and not url.startswith("https://api.mizabot."):
 				if url.startswith("https://csnftcg.mizabot."):
 					raise cp.HTTPRedirect(f"https://csnftcg.mizabot.xyz:9604/{url.rsplit('/', 1)[-1]}", 307)
 				# time.sleep(10)
@@ -290,7 +290,7 @@ if os.path.exists("domain.cert.pem") and os.path.exists("private.key.pem"):
 		return decorator
 	def e404(status, message, traceback, version):
 		url = cp.url(qs=cp.request.query_string)
-		if not url.startswith("https://mizabot.") and not url.startswith("https://i.mizabot."):
+		if not url.startswith("https://mizabot.") and not url.startswith("https://api.mizabot."):
 			print("Not Found:", url)
 			time.sleep(3600)
 		return message
@@ -1343,7 +1343,7 @@ transform: translate(-50%, -50%);
 	@cp.expose(("index", "p", "preview", "files", "file", "chat", "tester", "atlas", "mizatlas", "user", "login", "logout", "mpinsights", "createredirect"))
 	@hostmap
 	def index(self, path=None, filename=None, *args, code=None, **kwargs):
-		url = cp.url(qs=cp.request.query_string)
+		url = cp.url(qs=cp.request.query_string).rstrip("/")
 		if "/user" in url:
 			try:
 				sessid = int(cp.request.cookie["sessid"].value)
@@ -1420,91 +1420,92 @@ transform: translate(-50%, -50%);
 		if "/upload" in url:
 			raise cp.HTTPRedirect(url.replace("/upload", "/files"), status=307)
 		data, mime = fetch_static("index.html")
-		meta = '<meta property="og:title" content="Miza"><meta property="og:description" content="A multipurpose Discord bot.">'
-		if "/file" in url or "/files" in url:
-			meta += '<meta property="og:image" content="/mizaleaf.png">'
-		else:
-			meta += '<meta property="og:image" content="/logo256.png">'
-		meta += '<meta property="og:site_name" content="Miza">'
-		if not xrand(2) and (dt := datetime.datetime.utcnow()) and (dt.month, dt.day) in ((3, 31), (4, 1), (4, 2)):
-			meta += f'<meta http-equiv="refresh" content={xrand(15, 31)};url=https://{cp.request.headers["Host"]}/teapot">'
-		if path:
-			ind = IND
-			p = None
-			if path.startswith("!"):
-				ind = "!"
-				path = path[1:]
-			elif not path.startswith("@"):
-				b = path.lstrip("~").split(".", 1)[0].encode("utf-8") + b"=="
-				if b.startswith(b"dQ"):
-					c = b[2:]
-					path = str(p2n(c))
-					try:
-						p = find_file(path, ind=ind)
-					except FileNotFoundError:
-						pass
-					else:
-						url = cp.request.base + "/i/" + c.rstrip(b"=").decode("utf-8", "replace") + ".gif"
-						return f"""<!DOCTYPE html>
-<html><head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7025724554077000" crossorigin="anonymous"></script>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta property="og:url" content="/">
-<meta property="og:type" content="video.other">
-<meta property="twitter:player" content="https://www.youtube.com/embed/dQw4w9WgXcQ">
-<meta property="og:video:type" content="text/html">
-<meta property="og:video:width" content="960">
-<meta property="og:video:height" content="720">
-<meta name="twitter:image" content="{url}">
-<meta http-equiv="refresh" content="0;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-</head><body></body></html>""".encode("utf-8")
-				path = str(p2n(b))
+		if url.split("//", 1)[-1].count("/") > 1:
+			meta = '<meta property="og:title" content="Miza"><meta property="og:description" content="A multipurpose Discord bot.">'
+			if "/file" in url or "/files" in url:
+				meta += '<meta property="og:image" content="/mizaleaf.png">'
 			else:
-				path = path[1:]
-			if not p:
-				p = find_file(path, cwd=("cache", "saves/filehost"), ind=ind)
-			sem = SEMAPHORES.get(p)
-			if not sem:
-				while len(SEMAPHORES) >= 4096:
-					sem = SEMAPHORES.pop(next(iter(SEMAPHORES)))
-					if sem.is_busy():
-						raise SemaphoreOverflowError
-				sem = SEMAPHORES[p] = Semaphore(256, 256, rate_limit=60)
-			with sem:
-				fn = p.rsplit("/", 1)[-1].split("~", 1)[-1].rstrip(IND)
-				attachment = filename or fn
-				a2 = url_unparse(attachment).removeprefix(".temp$@")
-				f_url = url.replace("/file/", "/f/")
-				mim = get_mime(p)
-				description = mim + f", {byte_scale(os.path.getsize(p))}B"
-				meta = '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">'
-				if mim.startswith("video/"):
-					i_url = url.replace("/file/", "/i/") + ".gif"
-					meta += f"""<meta property="og:type" content="video.other">\
-<meta property="twitter:player" content="{f_url}">\
-<meta property="og:video:type" content="{mim}">\
-<meta property="og:video:width" content="960">\
-<meta name="twitter:image" content="{i_url}">"""
+				meta += '<meta property="og:image" content="/logo256.png">'
+			meta += '<meta property="og:site_name" content="Miza">'
+			if not xrand(2) and (dt := datetime.datetime.utcnow()) and (dt.month, dt.day) in ((3, 31), (4, 1), (4, 2)):
+				meta += f'<meta http-equiv="refresh" content={xrand(15, 31)};url=https://{cp.request.headers["Host"]}/teapot">'
+			if path:
+				ind = IND
+				p = None
+				if path.startswith("!"):
+					ind = "!"
+					path = path[1:]
+				elif not path.startswith("@"):
+					b = path.lstrip("~").split(".", 1)[0].encode("utf-8") + b"=="
+					if b.startswith(b"dQ"):
+						c = b[2:]
+						path = str(p2n(c))
+						try:
+							p = find_file(path, ind=ind)
+						except FileNotFoundError:
+							pass
+						else:
+							url = cp.request.base + "/i/" + c.rstrip(b"=").decode("utf-8", "replace") + ".gif"
+							return f"""<!DOCTYPE html>
+	<html><head>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7025724554077000" crossorigin="anonymous"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta property="og:url" content="/">
+	<meta property="og:type" content="video.other">
+	<meta property="twitter:player" content="https://www.youtube.com/embed/dQw4w9WgXcQ">
+	<meta property="og:video:type" content="text/html">
+	<meta property="og:video:width" content="960">
+	<meta property="og:video:height" content="720">
+	<meta name="twitter:image" content="{url}">
+	<meta http-equiv="refresh" content="0;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+	</head><body></body></html>""".encode("utf-8")
+					path = str(p2n(b))
 				else:
-					i_url = url.replace("/file/", "/r/") + ".gif"
-					meta += f"""<meta name="twitter:image:src" content="{i_url}">\
-<meta name="twitter:card" content="summary_large_image">\
-<meta name="twitter:title" content="{a2}"><meta property="twitter:url" content="{f_url}"><meta property="og:image" content="{i_url}">\
-<meta property="og:image:type" content="{mim}"><meta property="og:url" content="{f_url}"><meta name="og:description" content="{description}">"""
-		else:
-			a2 = "Miza"
-			description = "A multipurpose Discord bot."
-		i = data.index(b'</title>') + 8
-		s = """<!doctype html><html lang="en"><head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7025724554077000" crossorigin="anonymous"></script>
-<meta charset="utf-8"/><link rel="icon" href="/logo256.png"/>\
-<meta charset="utf-8"><meta name="author" content="Miza">\
-<meta name="viewport" content="width=device-width,initial-scale=1"/>\
-<meta name="theme-color" content="#694777"/>\
-<link rel="apple-touch-icon" href="/logo256.png"/>\
-<link rel="manifest" href="/manifest.json"/>""" + meta
-		t = f'<title>{a2}</title><meta name="description" content="{description}"/>'
-		data = s.encode("utf-8") + t.encode("utf-8") + data[i:]
+					path = path[1:]
+				if not p:
+					p = find_file(path, cwd=("cache", "saves/filehost"), ind=ind)
+				sem = SEMAPHORES.get(p)
+				if not sem:
+					while len(SEMAPHORES) >= 4096:
+						sem = SEMAPHORES.pop(next(iter(SEMAPHORES)))
+						if sem.is_busy():
+							raise SemaphoreOverflowError
+					sem = SEMAPHORES[p] = Semaphore(256, 256, rate_limit=60)
+				with sem:
+					fn = p.rsplit("/", 1)[-1].split("~", 1)[-1].rstrip(IND)
+					attachment = filename or fn
+					a2 = url_unparse(attachment).removeprefix(".temp$@")
+					f_url = url.replace("/file/", "/f/")
+					mim = get_mime(p)
+					description = mim + f", {byte_scale(os.path.getsize(p))}B"
+					meta = '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">'
+					if mim.startswith("video/"):
+						i_url = url.replace("/file/", "/i/") + ".gif"
+						meta += f"""<meta property="og:type" content="video.other">\
+	<meta property="twitter:player" content="{f_url}">\
+	<meta property="og:video:type" content="{mim}">\
+	<meta property="og:video:width" content="960">\
+	<meta name="twitter:image" content="{i_url}">"""
+					else:
+						i_url = url.replace("/file/", "/r/") + ".gif"
+						meta += f"""<meta name="twitter:image:src" content="{i_url}">\
+	<meta name="twitter:card" content="summary_large_image">\
+	<meta name="twitter:title" content="{a2}"><meta property="twitter:url" content="{f_url}"><meta property="og:image" content="{i_url}">\
+	<meta property="og:image:type" content="{mim}"><meta property="og:url" content="{f_url}"><meta name="og:description" content="{description}">"""
+			else:
+				a2 = "Miza"
+				description = "A multipurpose Discord bot."
+			i = data.index(b'</title>') + 8
+			s = """<!doctype html><html lang="en"><head>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7025724554077000" crossorigin="anonymous"></script>
+	<meta charset="utf-8"/><link rel="icon" href="/logo256.png"/>\
+	<meta charset="utf-8"><meta name="author" content="Miza">\
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>\
+	<meta name="theme-color" content="#694777"/>\
+	<link rel="apple-touch-icon" href="/logo256.png"/>\
+	<link rel="manifest" href="/manifest.json"/>""" + meta
+			t = f'<title>{a2}</title><meta name="description" content="{description}"/>'
+			data = s.encode("utf-8") + t.encode("utf-8") + data[i:]
 		cp.response.headers.update(CHEADERS)
 		cp.response.headers["Content-Type"] = mime
 		cp.response.headers["Content-Length"] = len(data)
@@ -2722,7 +2723,7 @@ def ensure_parent(proc, parent):
 		time.sleep(6)
 
 if __name__ == "__main__":
-	# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+	logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s')
 	pid = os.getpid()
 	ppid = os.getppid()
 	send(f"Webserver starting on port {PORT}, with PID {pid} and parent PID {ppid}...")
