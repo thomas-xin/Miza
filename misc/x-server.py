a074226bd2d490892aa2684f4b5b6f09b8476666
Thomas Xin <thomasxin@gmail.com>
2023-04-26 18:44:35 +1000

    serve non-ranges separately

--- misc/x-server.py
+++ misc/x-server.py
@@ -768,11 +768,13 @@ transform: translate(-50%, -50%);
 								return resp
 							if info[1] > 64 * 1048576:
 								uri = f"{HOST}/fileinfo/{orig_path}"
-								url = choice(
-									"https://stream.miza-stream.workers.dev/",
-									"https://stream.sub-stream.workers.dev/",
-								) + f"?info={urllib.parse.quote_plus(uri)}"
-								# url = f"{HOST}/stream?info={urllib.parse.quote_plus(uri)}"
+								if cp.request.headers.get("Range"):
+									url = f"{HOST}/stream?info={urllib.parse.quote_plus(uri)}"
+								else:
+									url = choice(
+										"https://stream.miza-stream.workers.dev/",
+										"https://stream.sub-stream.workers.dev/",
+									) + f"?info={urllib.parse.quote_plus(uri)}"
 								raise cp.HTTPRedirect(url, status="307")
 								# return self.dyn_serve(urls, size=info[1])
 							return self.concat(p, urls, name=info[0], mime=info[2], stn=stn)
