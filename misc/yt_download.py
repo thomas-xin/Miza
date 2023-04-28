080ba1afc91dea06ea1f24f633e72c19491a9503
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-04-29 00:19:17 +1000

    Update yt_download.py

--- misc/yt_download.py
+++ misc/yt_download.py
@@ -30,7 +30,7 @@ def create_driver():
 	service = browser["service"](browser["path"])
 	options = browser["options"]()
 	options.add_argument("--headless")
-	# options.add_argument("--disable-gpu")
+	options.add_argument("--disable-gpu")
 	options.add_argument("--no-sandbox")
 	options.add_argument("--deny-permission-prompts")
 	options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
