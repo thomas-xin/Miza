47eb22610fe868d74465323bb65ffef1f39e14ef
Thomas Xin <30500701+thomas-xin@users.noreply.github.com>
2023-05-03 15:29:05 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2779,6 +2779,8 @@ class Bot(discord.Client, contextlib.AbstractContextManager, collections.abc.Cal
             if not expendable:
                 return 0
             while stats.free < 81 * 1073741824 or len(expendable) > 4096 or (t - expendable[0].stat().st_atime) > 3600 * 12:
+                if not expendable:
+                    break
                 with tracebacksuppressor:
                     os.remove(expendable.pop(0).path)
                     i += 1
