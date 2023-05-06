6772ea39550558f3079e95ae834d290d2e6b9cee
Thomas Xin <thomasxin@gmail.com>
2023-05-07 00:02:40 +1000

    remove debug again

--- commands/IMAGE.py
+++ commands/IMAGE.py
@@ -1539,7 +1539,7 @@ class Art(Command):
                     fut = create_task(process_image("IBASL", "&", [prompt, kwargs, nsfw, False, c], fix=2, timeout=1200))
             self.imagebot.token = oai or AUTH.get("openai_key")
             ims = await create_future(self.imagebot.art, prompt, url, url2, kwargs, specified, dalle2, openjourney, nsfw, amount - c, timeout=480)
-            print(ims)
+            # print(ims)
             if fut:
                 ims2 = await fut
                 ims.extend(ims2)
