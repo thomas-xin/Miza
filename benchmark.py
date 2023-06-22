e0761dde7bf35ac26db69580fc2adc7b5dde88f4
Thomas <thomasxin@gmail.com>
2023-06-23 03:00:36 +1000

    device-specific benchmarks

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,46 @@
+import sys
+if len(sys.argv) > 1:
+    device, name, core = sys.argv[1:]
+    print(f"Benchmarking device {device}...")
+    is_cuda = device.startswith("cuda:")
+    if is_cuda:
+        import os
+        os.environ["CUDA_VISIBLE_DEVICES"] = device.split(":", 1)[-1]
+        device = "cuda:0"
+    import torch, time
+    from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+    model = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16 if is_cuda else torch.float32)
+    if is_cuda:
+        pipe = pipe.to(device)
+    pipe.enable_attention_slicing()
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    if is_cuda:
+        try:
+            pipe.enable_model_cpu_offload()
+        except AttributeError:
+            pass
+    pipe.safety_checker = lambda images, **kwargs: (images, [False] * len(images))
+    count = 4
+    taken = 0
+    temp = []
+    mark = lambda *args: temp.append(time.time())
+    while True:
+        temp.clear()
+        data = pipe(" ".join(["water"] * 64), num_inference_steps=count, callback=mark)
+        delays = [temp[i] - temp[i - 1] for i in range(2, len(temp))]
+        taken = sum(delays)
+        if taken < 45:
+            count <<= 1
+            continue
+        break
+    tavg = taken
+    avg = count / tavg * 100000
+    print(f"Benchmarked {name} ({core}-core). Average time taken for {count} iteration(s): {tavg}")
+    print(f"Score: {round(avg, 2)}")
+    print(avg)
+    raise SystemExit
+
 print("Loading...")
 import pynvml
 try:
@@ -8,49 +51,21 @@ except:
 if not DC:
     print("WARNING: No NVIDIA GPUs detected. Please install one for AI compute acceleration.")
 
-import torch, time, cpuinfo
-compute_load = []
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
-model = "runwayml/stable-diffusion-v1-5"
+import cpuinfo, subprocess
 
-def bench(device, name, core):
-    print(f"Benchmarking device {device}...")
-    is_cuda = device.startswith("cuda:")
-    pipe = StableDiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16 if is_cuda else torch.float32)
-    if is_cuda:
-        pipe = pipe.to(device)
-    # pipe.enable_attention_slicing()
-    # pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    # if is_cuda:
-    #     try:
-    #         pipe.enable_model_cpu_offload()
-    #     except AttributeError:
-    #         pass
-    pipe.safety_checker = lambda images, **kwargs: (images, [False] * len(images))
-    count = 1
-    taken = 0
-    while True:
-        t = time.time()
-        data = pipe(" ".join(["water"] * 64), num_inference_steps=count)
-        taken = time.time() - t
-        if taken < 15:
-            count <<= 1
-            continue
-        break
-    for i in range(3):
-        t = time.time()
-        data = pipe(" ".join(["water"] * 64), num_inference_steps=count)
-        taken += time.time() - t
-    tavg = taken / 4
-    avg = count / tavg * 100000
-    print(f"Benchmarked {name} ({core}-core). Average time taken for {count} iteration(s): {tavg}")
-    print(f"Score: {round(avg, 2)}")
-    return avg
 
 total = 0
 info = cpuinfo.get_cpu_info()
-total += bench("cpu", info["brand_raw"], info["count"])
-for i in range(torch.cuda.device_count()):
+args = [sys.executable, sys.argv[0], "cpu", info["brand_raw"], str(info["count"])]
+proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+s = proc.stdout.readlines()
+total += float(s.pop(-1))
+print(b"\n".join(s).decode("utf-8"))
+for i in range(DC):
     info = pynvml.nvmlDeviceGetHandleByIndex(i)
-    total += bench(f"cuda:{i}", pynvml.nvmlDeviceGetName(info), pynvml.nvmlDeviceGetNumGpuCores(info))
+    args = [sys.executable, sys.argv[0], f"cuda:{i}", pynvml.nvmlDeviceGetName(info), str(pynvml.nvmlDeviceGetNumGpuCores(info))]
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+    s = proc.stdout.readlines()
+    total += float(s.pop(-1))
+    print(b"\n".join(s).decode("utf-8"))
 print(f"Benchmark complete. Total score: {round(total, 2)}")
