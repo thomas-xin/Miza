ae9e3059975846eb834830dc995ff4838ad874a2
Thomas <thomasxin@gmail.com>
2023-07-18 13:54:34 +1000

    oops

--- commands/IMAGE.py
+++ commands/IMAGE.py
@@ -1643,13 +1643,13 @@ class Art(Command):
             openjourney = "journey" in name
             with discord.context_managers.Typing(channel):
                 futt = []
-                # c = 0
+                c = 0
                 # if not dalle2 and not openjourney and not url and not self.sdiff_sem.is_busy():
                 #     c = min(amount, 9 if nsfw and not self.sdiff_sem.active else 5)
                 #     c2 = c
                 #     while c2 > 0:
                 #         n = min(c2, xrand(floor(sqrt(c2) + 1)) + 1)
-                #         fut = create_task(process_image("IBASL", "&", [prompt, kwargs, nsfw, False, n], fix=3, pwr=1000000, timeout=120))
+                #         fut = create_task(process_image("IBASL", "&", [prompt, kwargs, nsfw, False, n], fix=3, pwr=250000 * n, timeout=120))
                 #         futt.append(fut)
                 #         c2 -= n
                 self.imagebot.token = oai or AUTH.get("openai_key")
@@ -1838,7 +1838,7 @@ class Art(Command):
                             c2 = c
                             while c2 > 0:
                                 n = min(c2, xrand(floor(sqrt(c2) + 1)) + 1)
-                                fut = create_task(process_image("IBASL", "&", [p, kwargs, nsfw, False, n], fix=3, pwr=1000000, timeout=120))
+                                fut = create_task(process_image("IBASL", "&", [p, kwargs, nsfw, False, n], fix=3, pwr=250000 * n, timeout=120))
                                 futt.append(fut)
                                 c2 -= n
                             for fut in futt:
