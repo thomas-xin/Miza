754ef17e03df340b03b2aa3ef3cfc8eae05c1d94
Thomas <thomasxin@gmail.com>
2023-10-30 16:37:32 +1100

    did I forget anything?

--- smath.py
+++ smath.py
@@ -2515,9 +2515,9 @@ else:
 	randbytes = lambda size: np.random.randint(0, 256, size=size, dtype=np.uint8).data
 
 # SHA256 operations: base64 and base16.
-shash = lambda s: as_str(base64.urlsafe_b64encode(hashlib.sha256(s if type(s) is bytes else as_str(s).encode("utf-8")).digest()).rstrip(b"="))
+shash = lambda s: base64.urlsafe_b64encode(hashlib.sha256(s if type(s) is bytes else as_str(s).encode("utf-8")).digest()).rstrip(b"=").decode("ascii")
 uhash = lambda s: shash(s) if len(s) > 43 else s
-hhash = lambda s: bytes2hex(hashlib.sha256(s if type(s) is bytes else as_str(s).encode("utf-8")).digest(), space=False)
+hhash = lambda s: hashlib.sha256(s if type(s) is bytes else as_str(s).encode("utf-8")).hexdigest()
 ihash = lambda s: int.from_bytes(hashlib.md5(s if type(s) is bytes else as_str(s).encode("utf-8")).digest(), "little") % 4294967296 - 2147483648
 nhash = lambda s: int.from_bytes(hashlib.md5(s if type(s) is bytes else as_str(s).encode("utf-8")).digest(), "little")
 
